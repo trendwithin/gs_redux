c975803320d2ca738d958b47c2170d07fba6f748
Michael Becco <e4e6d4d5@gmail.com>
2015-04-17 09:04:30 -0700

    Add live view to form. Add validation to form

--- app.js
+++ app.js
@@ -5,6 +5,15 @@
     this.products = gems;
   });
 
+  app.controller("ReviewController", function(){
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
   app.controller('PanelController', function() {
     this.tab = 1;
 
